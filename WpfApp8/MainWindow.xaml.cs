ca9b70337092ebe8533380418b0d38c9c03faf80
alexey951 <leshamedvedev60@gmail.com>
2022-11-12 21:39:46 +0300

    avtoriz\reg
    avtoriz\reg

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new ApplicationContext();
+
+            DoubleAnimation btnAnimation = new DoubleAnimation();
+            btnAnimation.From = 0;
+            btnAnimation.To = 450;
+            btnAnimation.Duration = TimeSpan.FromSeconds(1.5);
+            regButton.BeginAnimation(Button.WidthProperty, btnAnimation);
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass_2 = passBox_2.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if(login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Это поле введено не корректно!";
+                textBoxLogin.Background = Brushes.Red;
+            } 
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Это поле введено не корректно!";
+                passBox.Background = Brushes.Red;
+            }
+            else if (pass != pass_2)
+            {
+                passBox_2.ToolTip = "Это поле введено не корректно!";
+                passBox_2.Background = Brushes.Red;
+            }
+            else if(email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Это поле введено не корректно!";
+                textBoxEmail.Background = Brushes.Red;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBox_2.ToolTip = "";
+                passBox_2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Всё хорошо!");
+
+                User user = new User(login, email, pass);
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                AuthWindow authWindow = new AuthWindow();
+                authWindow.Show();
+                Hide();
+            }
+        }
+
+        private void Button_Window_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+            Hide();
+        }
+    }
+}
